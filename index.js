6a7bb92fb18f6c6532ebbd73e5417105939f9cbc
shriyaaditya <148246201+shriyaaditya@users.noreply.github.com>
2024-02-14 16:21:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+let popup=document.getElementById("popup");
+function openpopup(){
+  popup.classList.add("open-popup");
+}
+function closepopup(){
+  popup.classList.remove("open-popup");
+}
+let popup2=document.getElementById("popup2");
+function openpopup2(){
+  popup2.classList.add("open-popup2");
+}
+function closepopup2(){
+  popup2.classList.remove("open-popup2");
+}
+function login(){
+  location.assign("page.html")
+}
